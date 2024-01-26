ac4d95667d3369459a177622737a7dbff2ec1bc8
rachel648 <rachelraqelmurithi@gmail.com>
2024-01-26 03:08:37 -0800

    l

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,37 +1,29 @@
 #!/usr/bin/python3
-
 """
-Python script that, using a REST API, for a given employee ID,
-returns information about his/her TODO list progress.
+A Python script using a REST API, for a given employee ID,
+returns information about his/her TODO list progress
 """
 
-from requests import get
-from sys import argv
+import requests
+import sys
 
 
 if __name__ == "__main__":
-    response = get('https://jsonplaceholder.typicode.com/todos/')
-    data = response.json()
-    completed = 0
-    total = 0
-    tasks = []
-    response2 = get('https://jsonplaceholder.typicode.com/users')
-    data2 = response2.json()
-
-    for i in data2:
-        if i.get('id') == int(argv[1]):
-            employee = i.get('name')
-
-    for i in data:
-        if i.get('userId') == int(argv[1]):
-            total += 1
-
-            if i.get('completed') is True:
-                completed += 1
-                tasks.append(i.get('title'))
-
-    print("Employee {} is done with tasks({}/{}):".format(employee, completed,
-                                                          total))
-
-    for i in tasks:
-        print("\t {}".format(i))
+    users = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                         .format(sys.argv[1]))
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
+                         .format(sys.argv[1]))
+    dict_users = users.json()
+    dict_todos = todos.json()
+    comp_tasks = 0
+    tasks = 0
+    for i in dict_todos:
+        if i.get('completed') is True:
+            comp_tasks += 1
+        else:
+            tasks += 1
+    print("Employee {} is done with tasks({}/{}):"
+          .format(dict_users['name'], comp_tasks, tasks + comp_tasks))
+    for i in dict_todos:
+        if i.get('completed') is True:
+            print('\t {}'.format(i.get('title')))
