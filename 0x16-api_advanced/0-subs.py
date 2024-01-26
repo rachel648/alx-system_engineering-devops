eb715018ae94f9eca88e7a735e5638a13d4adfa6
rachel648 <rachelraqelmurithi@gmail.com>
2024-01-26 02:59:11 -0800

    l

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,27 +1,17 @@
 #!/usr/bin/python3
-"""
-Importing requests module
-"""
+"""A function that queries the Reddit API and returns the number of
+subscribers (not active users, total subscribers) for a given subreddit"""
 
-from requests import get
+import requests
 
 
 def number_of_subscribers(subreddit):
-    """
-    function that queries the Reddit API and returns the number of subscribers
-    (not active users, total subscribers) for a given subreddit.
-    """
-
-    if subreddit is None or not isinstance(subreddit, str):
+    """returns the number of subscribers from the Reddit API"""
+    r = requests.get(r'https://www.reddit.com/r/{}/about.json'
+                     .format(subreddit), headers={'User-agent': 'x'},
+                     allow_redirects=False)
+    if r.status_code != 200:
         return 0
-
-    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    response = get(url, headers=user_agent)
-    all_data = response.json()
-
-    try:
-        return all_data.get('data').get('subscribers')
-
-    except:
-        return 0
+    json = r.json()
+    data = json.get('data')
+    return data.get('subscribers', 0)
